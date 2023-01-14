e25d82a413eaef8f8f4969088e9ab7bb0cb94f72
BuildTools <unconfigured@null.spigotmc.org>
2023-01-14 18:05:47 -0500

    remove bin folders

--- plugin/bin/main/com/dominikkorsa/discordintegration/exception/ConfigNotSetException.kt
+++
@@ -1,5 +0,0 @@
-package com.dominikkorsa.discordintegration.exception
-
-class ConfigNotSetException(route: String) : Exception("Field $route not set") {
-    constructor(parent: String?, route: String) : this(parent?.let { "$it.$route" } ?: route)
-}
