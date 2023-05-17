88aab2ce0457309a37fa3788f7a1bd75ddfb077b
Dominik Korsa <dominik.korsa@gmail.com>
2023-05-17 09:57:15 +0200

    Handle 404 in Mojang username API response

--- plugin/src/main/kotlin/com/dominikkorsa/discordintegration/AvatarService.kt
+++ plugin/src/main/kotlin/com/dominikkorsa/discordintegration/AvatarService.kt
@@ -4,6 +4,7 @@ import com.dominikkorsa.discordintegration.response.NameToUUIDResponse
 import io.ktor.client.*
 import io.ktor.client.call.*
 import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.*
 import io.ktor.client.plugins.contentnegotiation.*
 import io.ktor.client.request.*
 import io.ktor.http.*
@@ -15,6 +16,7 @@ import java.util.*
 class AvatarService(private val plugin: DiscordIntegration) {
     private val uuids = HashMap<String, String>()
     private val client =  HttpClient(CIO) {
+        expectSuccess = true
         install(ContentNegotiation) {
             json(Json {
                 ignoreUnknownKeys = true
@@ -27,9 +29,10 @@ class AvatarService(private val plugin: DiscordIntegration) {
             val response = client.get(
                 "https://api.mojang.com/users/profiles/minecraft/${playerName}"
             )
-            if (response.status == HttpStatusCode.NoContent) return
             uuids[playerName] = response.body<NameToUUIDResponse>().id
         } catch (error: Exception) {
+            if (error is ClientRequestException && error.response.status == HttpStatusCode.NoContent) return
+            if (error is ClientRequestException && error.response.status == HttpStatusCode.NotFound) return
             plugin.logger.warning("Failed to get UUID of player $playerName")
             plugin.logger.warning(error.message)
         }
