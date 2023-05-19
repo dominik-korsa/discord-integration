e39b012e2cd89837f1e48f3581573d5c64a3cead
Dominik Korsa <dominik.korsa@gmail.com>
2023-05-19 10:42:45 +0200

    Fix #62

--- plugin/src/main/kotlin/com/dominikkorsa/discordintegration/luckperms/LuckPerms.kt
+++ plugin/src/main/kotlin/com/dominikkorsa/discordintegration/luckperms/LuckPerms.kt
@@ -3,12 +3,14 @@ package com.dominikkorsa.discordintegration.luckperms
 import com.dominikkorsa.discordintegration.DiscordIntegration
 import net.luckperms.api.LuckPerms
 import org.bukkit.Bukkit
-import org.bukkit.plugin.RegisteredServiceProvider
 
 fun registerLuckPerms(plugin: DiscordIntegration): Boolean {
-    val provider: RegisteredServiceProvider<LuckPerms> =
-        Bukkit.getServicesManager().getRegistration(LuckPerms::class.java) ?: return false
-    val api: LuckPerms = provider.provider
-    api.contextManager.registerCalculator(LinkedContextCalculator(plugin))
+    val luckPermsClass = try {
+        LuckPerms::class.java
+    } catch (error: NoClassDefFoundError) {
+        return false
+    }
+    val registration = Bukkit.getServicesManager().getRegistration(luckPermsClass) ?: return false
+    registration.provider.contextManager.registerCalculator(LinkedContextCalculator(plugin))
     return true
 }
