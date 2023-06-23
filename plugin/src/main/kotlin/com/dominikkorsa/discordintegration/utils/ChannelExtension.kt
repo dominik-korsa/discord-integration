1c9a96be708f0cbe1bf07f4ddf6b772209c14867
Dominik Korsa <dominik.korsa@gmail.com>
2023-06-23 16:13:00 +0200

    Update Discord4J to v3.3.0-M2

--- plugin/src/main/kotlin/com/dominikkorsa/discordintegration/utils/ChannelExtension.kt
+++ plugin/src/main/kotlin/com/dominikkorsa/discordintegration/utils/ChannelExtension.kt
@@ -1,11 +1,11 @@
 package com.dominikkorsa.discordintegration.utils
 
-import discord4j.core.`object`.entity.channel.GuildMessageChannel
+import discord4j.core.`object`.entity.channel.TopLevelGuildMessageChannel
 import discord4j.core.util.PermissionUtil
 import discord4j.rest.util.PermissionSet
 import kotlinx.coroutines.reactive.awaitSingle
 
-suspend fun GuildMessageChannel.getEffectiveEveryonePermissions(): PermissionSet {
+suspend fun TopLevelGuildMessageChannel.getEffectiveEveryonePermissions(): PermissionSet {
     val everyoneRole = guild.awaitSingle().everyoneRole.awaitSingle()
     return PermissionUtil.computePermissions(
         guild.awaitSingle().everyoneRole.awaitSingle().permissions,
